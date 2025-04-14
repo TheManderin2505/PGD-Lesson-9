b52dd7c1e022597c120e61a884c589ca890e2130
TheManderin2505 <Alexdotdriscoll@gmail.com>
2025-04-14 18:50:03 +0100

    Add files via upload

--- Lesson 9/main.py
+++ Lesson 9/main.py
@@ -9,11 +9,63 @@ SCREENHEIGHT = 600
 SCREEN = pygame.display.set_mode([SCREENWIDTH,SCREENHEIGHT])
 pygame.display.set_caption("Sprite Test 1")
 
-while True:
-    pygame.display.update()
+class Player(pygame.sprite.Sprite):
+    #property
+    def __init__(self):
+        #super extracts stuff from sprite
+        super().__init__()
+        self.image = pygame.image.load("rocket.png")
+        self.image = pygame.transform.scale(self.image,(70,100))
+        self.rect =  self.image.get_rect()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-        
-        #if event.type == pygame.
+        #function
+        def update(self,keys_pressed):
+            if(keys_pressed[pygame.K_UP]):
+                self.rect.move_ip(0,-5)
+
+            if(keys_pressed[pygame.K_DOWN]):
+                self.rect.move_ip(0,5)
+
+            if(keys_pressed[pygame.K_LEFT]):
+                self.rect.move_ip(-5,0)
+            
+            if(keys_pressed[pygame.K_RIGHT]):
+                self.rect.move_ip(5,0)
+
+            #Boundary
+            if self.rect.left < 0:
+                self.rect.left = 0
+            
+            if self.rect.right > SCREENWIDTH:
+                self.rect.right = SCREENWIDTH
+
+            if self.rect.top < 0:
+                self.rect.top = 0
+            
+            if self.rect.bottom > SCREENHEIGHT:
+                self.rect.bottom= SCREENHEIGHT
+
+#create a group
+
+spriteg = pygame.sprite.Group()            
+            
+def game_start():
+
+    while True:
+        #create object
+        rocket = Player()
+        spriteg.add(rocket)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        keys_pressed= pygame.key.get_pressed()
+        rocket.update(keys_pressed)
+
+        SCREEN.blit(pygame.image.load("space.png"),(0,0))
+        spriteg.draw(SCREEN)  
+        pygame.display.update()
+
+
+game_start()
